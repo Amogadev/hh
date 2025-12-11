93d34a9078a4ec1ac6e3d71020c68c24ea60d14f
ECEA 2021 <ecea2025@gmail.com>
2025-12-11 14:56:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-807408636-56e85",
+  "appId": "1:893590289660:web:c454cc031a1402690aab79",
+  "apiKey": "AIzaSyB0zNXGrj4ui9fE_BuhOLKM5JrFxIOIWHI",
+  "authDomain": "studio-807408636-56e85.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "893590289660"
+};
